079221961a2f40b72ebadf953d3c28450c3ece1a
brenno harten <brennoharten@gmail.com>
2021-02-26 20:13:32 -0300

    fixed bugs on header

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,9 @@ jQuery("document").ready(function($){
     window.onscroll = function () {
         if(window.pageYOffset > 140) {
             $("#header").addClass("active");
+            $("#header .navbar-toggler").addClass("collapsed");
+            $("#navbarNav").removeClass("show");
+            
         } else {
             $("#header").removeClass("active");
         }
