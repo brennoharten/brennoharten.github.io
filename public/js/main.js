c1dbf66aa7672af41b0622135087e3c478b2818c
brenno harten <brennoharten@gmail.com>
2021-02-28 17:19:16 -0300

    fixed projects

--- public/js/main.js
+++ public/js/main.js
@@ -11,4 +11,26 @@ jQuery("document").ready(function($){
         }
     }
 
+  //OWL
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 30,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        dots: true,
+        lazyLoad: true,
+        nav: false,
+        responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 1,
+        },
+        1000: {
+            items: 3,
+            },
+        },
+    });
 });
+
